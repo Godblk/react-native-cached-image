684f6a46336a4ed0ad6a243dc7a07f18e922cef2
张行舟 <hazel@ub-am.com>
2019-06-07 14:45:38 +0800

    屏蔽加载等待框

--- CachedImage.js
+++ CachedImage.js
@@ -164,9 +164,9 @@ class CachedImage extends React.Component {
     }
 
     render() {
-        if (this.state.isCacheable && !this.state.cachedImagePath) {
-            return this.renderLoader();
-        }
+        // if (this.state.isCacheable && !this.state.cachedImagePath) {
+        //     return this.renderLoader();
+        // }
         const props = getImageProps(this.props);
         const style = this.props.style || styles.image;
         const source = (this.state.isCacheable && this.state.cachedImagePath) ? {
